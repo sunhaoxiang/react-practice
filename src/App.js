3ba470c154c36b70e90effa36a0765cbbcc9e381
孙昊翔 <673686754@qq.com>
2018-01-02 21:52:51 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Learning</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Let's learn JSX. <span>{sayhi('you')}</span></p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function sayhi(name) {
+  return `Hi,${name}`
+}
+
+export default App
