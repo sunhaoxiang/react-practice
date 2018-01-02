090fe4e71a977fcef12fdbb3ddd0a05489f7cd94
孙昊翔 <673686754@qq.com>
2018-01-02 22:17:35 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  sayhi (name) {
+    return `Hi,${name}`
+  }
   render () {
     return (
       <div className="App">
@@ -10,14 +13,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Learning</h1>
         </header>
-        <p className="App-intro">Let's learn JSX. <span>{sayhi('you')}</span></p>
+        <p className="App-intro">Let's learn JSX. <span>{this.sayhi('you')}</span></p>
       </div>
     )
   }
-}
-
-function sayhi(name) {
-  return `Hi,${name}`
 }
 
 export default App
