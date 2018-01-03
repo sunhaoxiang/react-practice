d0e1eb8c751d37a0c7ddc990c79294dd15ce7c36
孙昊翔 <673686754@qq.com>
2018-01-03 10:22:10 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      home: 'luoyang'
+    }
+  }
+
+  // 箭头函数this指向外层，不需要bind(this)
+  changeHome = () => {
+    this.setState({
+      home: 'beijing'
+    })
+  }
+
+  // 需要bind(this)
+  // changeHome () {
+  //   this.setState({
+  //     home: 'beijing'
+  //   })
+  // }
   sayhi (name) {
     return `Hi,${name}`
   }
@@ -13,7 +33,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Learning</h1>
         </header>
-        <p className="App-intro">Let's learn JSX. <span>{this.sayhi('you')}</span></p>
+        <p className="App-intro">Let's learn JSX. <span>{this.sayhi('sunhaoxiang')}</span></p>
+        <p className="App-intro"onClick={this.changeHome}>home:{this.state.home}</p>
       </div>
     )
   }
